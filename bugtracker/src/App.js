58f9660fe92cbe86e83fc491ab719752e8bb5e68
NNcrawler <rasyid.xyz@gmail.com>
2017-11-13 19:24:45 +0700

    bug reporter migrated

--- bugtracker/src/App.js
+++ bugtracker/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BugReporter from './BugReporter';
+import Tasks from './Tasks';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App container">
+        <h1 className="title is-1">Bug Tracker <small>by HACKTIV8</small></h1>
+
+        <BugReporter/>
+
+        <Tasks/>
+
       </div>
     );
   }
---
+++ bugtracker/src/Tasks.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tasks extends Component {
+  render() {
+    return (
+      <div className="columns">
+        <div className="column is-medium" id="listBugs"></div>
+      </div>
+    )
+  }
+}
+
+export default Tasks;
