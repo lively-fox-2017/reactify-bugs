727efc463bf2e69783ac5b9cf3e0abc1ac67fe06
RizQjoe <rjoelisman@gmail.com>
2017-11-14 09:12:50 +0700

    update masih ada bug error

---
+++ bugtracker/src/components/Headers.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Headers(){
+    return <h1 className="title is-1">
+        Bug Tracker <small>by HACKTIV8</small>
+      </h1>
+}
+export default Headers
---
+++ bugtracker/src/components/List.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <div className="columns">
+        <div className="column is-medium" id="listBugs" />
+      </div>
+    );
+  }
+}
+
+export default List;
