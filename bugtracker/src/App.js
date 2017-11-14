ae39c4fc861907512b015f92d8091249cf84cec2
nach9 <nur.achdiansyah@gmail.com>
2017-11-14 08:53:51 +0700

    separate component

--- bugtracker/src/App.js
+++ bugtracker/src/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import { Container, Title } from 'reactbulma'
+import BugForm from './BugForm'
+import BugList from './BugList'
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Title>
+          Bug Tracker <small>by HACKTIV8</small>
+        </Title>
+        <BugForm/>
+        <hr />
+        <BugList/>
+      </Container>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
